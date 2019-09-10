95f744b9ff917eb82edcf49c71fc4d83fd702e8d
Brian Murphy <32182553+bmurdata@users.noreply.github.com>
2019-09-10 19:27:21 -0400

    Update of sentan adn tweetgatherer
    Update to show some sentiment analysis. Updated Positive and Negative vocabularies

--- Mining-Demo/tweetgatherer.py
+++ Mining-Demo/tweetgatherer.py
@@ -13,30 +13,37 @@ import operator
 from collections import Counter
 
 # Time limit set to 20 seconds, if undefined defaults to 60
-tweetstream = tweepy.Stream(auth, MyListener(time_limit=20))
-tweetstream.filter(track=['#python'])
+#tweetstream = tweepy.Stream(auth, MyListener(time_limit=20))
+#tweetstream.filter(track=['#python'])
 
 from collections import defaultdict
 # remember to include the other import from the previous post
- 
+
 com = defaultdict(lambda : defaultdict(int))
 fname='tweetstream.json'
 # Open and analyze the Tweets we pulled.
 
 
 with open(fname, 'r') as f:
-    for line in f:
-        tweet = json.loads(line)
-        terms_only = [term for term in preprocess(tweet['text']) 
+    
+       line = f.readline()
+
+       while line:
+            tweet=json.loads(line)
+            
+            line = f.readline()
+            tweet = json.loads(line)
+            terms_only = [term for term in preprocess(tweet['text']) 
                     if term not in stop
                     and not term.startswith(('#', '@'))]
     
         # Build co-occurrence matrix
-        for i in range(len(terms_only)-1):            
-            for j in range(i+1, len(terms_only)):
-                w1, w2 = sorted([terms_only[i], terms_only[j]])                
-                if w1 != w2:
-                    com[w1][w2] += 1
+            for i in range(len(terms_only)-1):            
+                for j in range(i+1, len(terms_only)):
+                    w1, w2 = sorted([terms_only[i], terms_only[j]])                
+                    if w1 != w2:
+                        com[w1][w2] += 1
+
 
 com_max = []
 # For each term, look for the most common co-occurrent terms
@@ -48,15 +55,14 @@ for t1 in com:
 terms_max = sorted(com_max, key=operator.itemgetter(1), reverse=True)
 print(terms_max[:5])
 
-search_word = 'Python' # pass a term as a command-line argument
+search_word = 'python' # pass a term as a command-line argument
 count_search = Counter()
-with open(fname, 'r') as f:
-    for line in f:
-        tweet = json.loads(line)
-        terms_only = [term for term in preprocess(tweet['text']) 
-                    if term not in stop 
-                    and not term.startswith(('#', '@'))]
-        if search_word in terms_only:
-            count_search.update(terms_only)
-    print("Co-occurrence for %s:" % search_word)
-    print(count_search.most_common(20))
+for line in f:
+    tweet = json.loads(line)
+    terms_only = [term for term in preprocess(tweet['text']) 
+                  if term not in stop 
+                  and not term.startswith(('#', '@'))]
+    if search_word in terms_only:
+        count_search.update(terms_only)
+print("Co-occurrence for %s:" % search_word)
+print(count_search.most_common(20))
